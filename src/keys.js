cdb63a45cf63fdcdabb11e6d8487ce110bbf6cfc
Pavlovi <milan.pavel1@gmail.com>
2018-10-24 10:30:01 +0200

    another test

--- src/keys.js
+++ src/keys.js
@@ -1,7 +1,7 @@
-if (process.env.NODE_ENV === "production") {
-  console.log("NODE_ENV === production");
-  module.exports = require("./firebase-prod");
-}
+// if (process.env.NODE_ENV === "production") {
+//   console.log("NODE_ENV === production");
+module.exports = require("./firebase-prod");
+// }
 // else {
 //   console.log("NODE_ENV === else!!!!");
 //   module.exports = require("./firebase");
