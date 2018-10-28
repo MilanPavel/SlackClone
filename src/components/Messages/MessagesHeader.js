84855fc034a70ec0ffe044f9f39de5f007254e76
Pavlovi <milan.pavel1@gmail.com>
2018-10-28 11:22:42 +0100

    add channel title/user count to message header

--- src/components/Messages/MessagesHeader.js
+++ src/components/Messages/MessagesHeader.js
@@ -1,18 +1,19 @@
 import React from "react";
 import { Header, Segment, Input, Icon } from "semantic-ui-react";
-// import HeaderSubHeader from "semantic-ui-react/dist/commonjs/elements/Header/HeaderSubheader";
 
 class MessagesHeader extends React.Component {
   render() {
+    const { channelName, numUniqueUsers } = this.props;
+
     return (
       <Segment clearing>
         {/* Channle Title */}
         <Header fluid="true" as="h2" floated="left" style={{ marginBottom: 0 }}>
           <span>
-            Channel
+            {channelName}
             <Icon name={"star outline"} color="black" />
           </span>
-          <Header.Subheader>2 Users</Header.Subheader>
+          <Header.Subheader>{numUniqueUsers}</Header.Subheader>
         </Header>
         {/* Channel Search Input */}
         <Header floated="right">
